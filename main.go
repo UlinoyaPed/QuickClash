9327ea6499627c963b32301f3341492f6b453be3
UlinoyaPed <ulinoyaped@gmail.com>
2023-08-14 12:08:37 +0800

    feat: 若配置文件存在且修改时间小于6小时则不进行下载
    chore: 更换WebUI为Yacd-Meta

--- main.go
+++ main.go
@@ -21,6 +21,7 @@ func init() {
 	_, err := os.Stat(QuickClashYml)
 	// 如果文件不存在则下载
 	if err != nil {
+		color.FgLightBlue.Println("配置文件不存在，正在下载...")
 		Download(RepoBaseUrl+QuickClashYml, QuickClashYml)
 	}
 
@@ -40,6 +41,7 @@ func cancelProxy() {
 }
 
 func main() {
+	var err error
 	//设置Gin为发布模式
 	gin.SetMode(gin.ReleaseMode)
 	// 创建一个Gin实例
@@ -53,19 +55,39 @@ func main() {
 	GinPort := fmt.Sprintf(":%s", config.String("quickclash.port"))
 	// 使用协程启动Gin服务
 	go func() {
-		if err := r.Run(GinPort); err != nil {
+		if err = r.Run(GinPort); err != nil {
 			panic(err)
 		}
 	}()
 
 	//		主协程可以继续执行其他操作
 
-	color.BgLightBlue.Println("正在下载配置文件，请稍等")
-	err := Download(config.String("quickclash.sublink"), QuickClashSubYml)
-	if err != nil {
-		panic(err)
+	//检查配置文件是否存在
+	_, errStat := os.Stat(QuickClashSubYml)
+	var duration time.Duration
+	if errStat == nil {
+		//检查文件修改时间
+		fileInfo, _ := os.Stat(QuickClashSubYml)
+		modTime := fileInfo.ModTime()
+		duration = time.Since(modTime)
 	}
-	color.BgLightBlue.Println("文件下载完成！")
+
+	// 如果文件不存在或超过6小时则下载
+	if errStat != nil || duration.Hours() > 6 {
+		SubUrl = config.String("quickclash.sublink")
+		if SubUrl == "" {
+			color.BgLightBlue.Println("您没有填写 quickclash.sublink 字段，请在此处输入")
+			fmt.Scanln(&SubUrl)
+		}
+		color.BgLightBlue.Println("正在下载配置文件，请稍等")
+		err = Download(SubUrl, QuickClashSubYml)
+		if err != nil {
+			panic(err)
+		}
+		color.BgLightBlue.Println("文件下载完成！")
+	} else {
+		color.BgLightBlue.Println("配置文件无需更新")
+	}
 
 	//加载配置文件
 	err = config.LoadFiles(QuickClashSubYml)
@@ -76,11 +98,9 @@ func main() {
 	//检查内核
 	if config.String("quickclash.core") == "clash" {
 		Core = ClashCore
-		CoreUrl = ClashUrl
 		color.BgLightBlue.Println("当前为 Clash 内核")
 	} else if config.String("quickclash.core") == "clash.meta" {
 		Core = ClashMetaCore
-		CoreUrl = ClashMetaUrl
 		color.BgLightBlue.Println("当前为 ClashMeta 内核")
 	} else {
 		panic(color.BgRed.Sprintf("内核设置有误，请检查 %s", QuickClashYml))
@@ -90,7 +110,11 @@ func main() {
 	_, err = os.Stat(Core)
 	// 如果文件不存在则下载
 	if err != nil {
-		Download(ReleaseBaseUrl+Core, Core)
+		color.FgLightBlue.Println("内核不存在，正在下载...")
+		err = Download(ReleaseBaseUrl+Core, Core)
+		if err != nil {
+			panic(err)
+		}
 	}
 
 	//启动Clash
@@ -112,8 +136,8 @@ func main() {
 	}
 
 	//提示信息
-	color.BgLightBlue.Printf("管理面板已启动，请访问localhost%s\n", GinPort)
-	color.BgLightBlue.Printf("管理面板 Base url：%s Secret：%s\n", "127.0.0.1"+config.String("external-controller"), config.String("quickclash.secret"))
+	color.BgLightBlue.Printf("管理面板 Base url：http://%s Secret：%s\n", "127.0.0.1"+config.String("external-controller"), config.String("quickclash.secret"))
+	color.BgLightRed.Printf("管理面板已启动，请访问 localhost%s\n", GinPort)
 
 	//		阻止主协程退出，以保持Gin服务的运行
 	select {}
--- vars.go
+++ vars.go
@@ -3,15 +3,13 @@ package main
 const (
 	QuickClashYml    = "config/QuickClash.yml"
 	RepoBaseUrl      = "https://ghproxy.com/https://gitgub.com/UlinoyaPed/QuickClash/raw/master/"
-	ReleaseBaseUrl   = ""
+	ReleaseBaseUrl   = "https://ghproxy.com/https://github.com/UlinoyaPed/QuickClash/releases/download/v0.1/"
 	QuickClashSubYml = "yaml/QuickClashSub.yaml"
 	ClashMetaCore    = "clash.meta.exe"
-	ClashMetaUrl     = ""
 	ClashCore        = "clash.exe"
-	ClashUrl         = ""
 )
 
 var (
-	Core    = ""
-	CoreUrl = ""
+	Core   = ""
+	SubUrl = ""
 )
