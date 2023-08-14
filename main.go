590bbbfd85503a3373b206b12f3120aee4c812e4
UlinoyaPed <ulinoyaped@gmail.com>
2023-08-14 10:53:30 +0800

    添加内核检测

--- main.go
+++ main.go
@@ -17,17 +17,18 @@ func init() {
 	// 添加驱动程序以支持yaml内容解析（除了JSON是默认支持，其他的则是按需使用）
 	config.AddDriver(yaml.Driver)
 
+	//检查配置文件是否存在
 	_, err := os.Stat(QuickClashYml)
-	if err == nil {
-		// 加载配置，可以同时传入多个文件
-		err := config.LoadFiles(QuickClashYml)
-		if err != nil {
-			panic(err)
-		}
-	} else {
+	// 如果文件不存在则下载
+	if err != nil {
 		Download(RepoBaseUrl+QuickClashYml, QuickClashYml)
 	}
 
+	// 加载配置，可以同时传入多个文件
+	err = config.LoadFiles(QuickClashYml)
+	if err != nil {
+		panic(err)
+	}
 }
 
 func cancelProxy() {
@@ -60,28 +61,49 @@ func main() {
 	//		主协程可以继续执行其他操作
 
 	color.BgLightBlue.Println("正在下载配置文件，请稍等")
-	err := Download(config.String("quickclash.sublink"), "yaml/QuickClashSub.yaml")
+	err := Download(config.String("quickclash.sublink"), QuickClashSubYml)
 	if err != nil {
 		panic(err)
 	}
 	color.BgLightBlue.Println("文件下载完成！")
 
 	//加载配置文件
-	err = config.LoadFiles("yaml/QuickClashSub.yaml")
+	err = config.LoadFiles(QuickClashSubYml)
 	if err != nil {
 		panic(err)
 	}
 
-	color.BgLightBlue.Println("正在开启clash")
-	command := "./clash.meta.exe"
-	params := []string{"-f", "yaml/QuickClashSub.yaml", "-secret", config.String("quickclash.secret")}
+	//检查内核
+	if config.String("quickclash.core") == "clash" {
+		Core = ClashCore
+		CoreUrl = ClashUrl
+		color.BgLightBlue.Println("当前为 Clash 内核")
+	} else if config.String("quickclash.core") == "clash.meta" {
+		Core = ClashMetaCore
+		CoreUrl = ClashMetaUrl
+		color.BgLightBlue.Println("当前为 ClashMeta 内核")
+	} else {
+		panic(color.BgRed.Sprintf("内核设置有误，请检查 %s", QuickClashYml))
+	}
+
+	//检查内核是否存在
+	_, err = os.Stat(Core)
+	// 如果文件不存在则下载
+	if err != nil {
+		Download(ReleaseBaseUrl+Core, Core)
+	}
+
+	//启动Clash
+	color.BgLightBlue.Println("正在开启Clash")
+	command := "./" + Core
+	params := []string{"-f", QuickClashSubYml, "-secret", config.String("quickclash.secret")}
 	go ExecCommand(command, params)
 
 	//等待2秒
 	time.Sleep(2 * time.Second)
-	proxy := fmt.Sprintf("127.0.0.1:%s", config.String("port"))
 
 	//设置系统代理
+	proxy := fmt.Sprintf("127.0.0.1:%s", config.String("port"))
 	defer cancelProxy()
 	if err := SetProxy(proxy); err == nil {
 		color.BgLightBlue.Printf("设置代理服务器: %s 成功!\n", proxy)
@@ -89,8 +111,10 @@ func main() {
 		color.BgRed.Printf("设置代理服务器: %s 失败, : %s\n", proxy, err)
 	}
 
+	//提示信息
 	color.BgLightBlue.Printf("管理面板已启动，请访问localhost%s\n", GinPort)
 	color.BgLightBlue.Printf("管理面板 Base url：%s Secret：%s\n", "127.0.0.1"+config.String("external-controller"), config.String("quickclash.secret"))
+
 	//		阻止主协程退出，以保持Gin服务的运行
 	select {}
 }
--- vars.go
+++ vars.go
@@ -1,6 +1,17 @@
 package main
 
 const (
-	QuickClashYml = "config/QuickClash.yml"
-	RepoBaseUrl   = "https://gitgub.com/UlinoyaPed/QuickClash/raw/master/"
+	QuickClashYml    = "config/QuickClash.yml"
+	RepoBaseUrl      = "https://ghproxy.com/https://gitgub.com/UlinoyaPed/QuickClash/raw/master/"
+	ReleaseBaseUrl   = ""
+	QuickClashSubYml = "yaml/QuickClashSub.yaml"
+	ClashMetaCore    = "clash.meta.exe"
+	ClashMetaUrl     = ""
+	ClashCore        = "clash.exe"
+	ClashUrl         = ""
+)
+
+var (
+	Core    = ""
+	CoreUrl = ""
 )
